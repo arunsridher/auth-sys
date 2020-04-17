8c42c86267d3bb3e064f89de5371baff56b3f324
arunsridher <45627741+arunsridher@users.noreply.github.com>
2020-04-17 18:45:20 +0530

    recaptcha for sign in

--- routes/users.js
+++ routes/users.js
@@ -33,11 +33,13 @@ verifyCaptcha = function(req, res, next){
 router.get('/sign-up', usersController.signUp);
 router.get('/sign-in', usersController.signIn);
 router.post('/create', recaptcha.middleware.verify = verifyCaptcha, usersController.createAccount);
+
 //use passport as a middleware to authenticate
-router.post('/create-session', passport.authenticate(
+router.post('/createSession',recaptcha.middleware.verify = verifyCaptcha, passport.authenticate(
   'local',
   {failureRedirect: '/users/sign-in'}
 ),usersController.createSession);
+
 router.get('/home', usersController.home);
 router.get('/sign-out', usersController.destroySession);
 router.post('/update-password', usersController.updatePassword);
@@ -45,19 +47,6 @@ router.get('/forgot-password', usersController.forgotPassword);
 router.post('/create-password-reset-req', usersController.createPasswordResetReq);
 router.get('/reset-password', usersController.resetPassword);
 router.post('/create-new-password', usersController.createNewPassword);
-
-
-captchaVerification = function (req, res, next) {
-  if (req.captcha.error) {
-      console.log(req.captcha.error);
-      req.flash('signupMessage','reCAPTCHA Incorrect');
-      res.redirect('back');
-  } else {
-    console.log("moving on")
-      return next();
-  }
-}
-
 
 //export router
 module.exports = router;
